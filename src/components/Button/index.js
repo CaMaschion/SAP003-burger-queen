f436d7b3a5c90c71bc2f1f1be5ea76af6261211f
Camila <cm.maschion@gmail.com>
2019-12-16 14:00:37 -0300

    components

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-const Button = ({onClick, text}) =>  <button onClick={onClick} className="button">{text}</button> 
-console.log('clicou')
+// const Button = ({onClick, text}) =>  <button onClick={onClick} className="button">{text}</button> 
+const Button = (props) =>  <button onClick={props.handleClick} className="button">{props.text}</button> 
+
 
 export default Button
 
--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,50 +1,23 @@
-import React, {useState, useEffect} from 'react'
-
-import firebase from '../../utils/firebase.js'
-//import { format } from 'util'
-
-function useMenu() {
-    const [menu, setMenu] = useState([])
-    
-    useEffect(() => {
-        firebase
-        .firestore()
-        .collection('menu')
-        .onSnapshot((snapshot) => {
-            const newMenu = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            }))
-
-            setMenu(newMenu)
-        })
-
-    }, [])
-
-    return menu
-
-}
+import React from 'react'
 
 
-const MenuList = () => {
-    const menu = useMenu()
+const MenuList = (props) => {
 
     return (
-        <div>
-            <h2>Faça seu pedido</h2>
-            <ol>
-                <h2>Menu</h2>
-                {menu.map((menu) =>
-                <li key="{menu.id}">                     
-                    <div className="burger">
-                        {menu.name}
-                    <h3 className="price">{menu.price}</h3>
-                    <button>adicionar ao pedido</button>  
-                    </div>
-                </li>         
-            )}   
-               </ol>
-        </div>                        
+       
+        <ol>
+           
+            {props.menuItens.map((menu) =>
+            <li key="{menu.id}">                     
+                <div className="burger">
+                    {menu.name}
+                <h3 className="price">{menu.price}</h3>
+                <button>adicionar ao pedido</button>  
+                </div>
+            </li>         
+        )}   
+            </ol>
+                             
 
     )
 }
